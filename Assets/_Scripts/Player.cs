9cf0a6bbf38f47ff1ec182e256864e20552a2992
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-08-19 12:43:01 +0300

    move

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,77 @@
+using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _jumpingPower;
+        [SerializeField] private LayerMask _groundLayerMask;
+        [SerializeField] private Collider2D _collider;
+
+        private bool m_IsFacingRight = true;
+        private bool m_IsGrounded;
+        private float m_HorizontalDirection;
+
+
+        private void Update()
+        {
+            SetHorizontalDirection();
+            
+            Flip();
+            Jump();
+            
+            CheckGround();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+        
+        
+        private void CheckGround()
+        {
+            m_IsGrounded = Physics2D.IsTouchingLayers(_collider, _groundLayerMask);
+
+        }
+        
+        private void Jump()
+        {
+            if (!m_IsGrounded) { return; }
+            
+            var velocity = _rigidbody2D.velocity;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody2D.velocity = new Vector2(velocity.x, _jumpingPower);
+            }
+        }
+        
+        private void SetHorizontalDirection()
+        {
+            m_HorizontalDirection = Input.GetAxisRaw("Horizontal");
+        }
+
+        private void Move()
+        {
+            var movement = new Vector2(m_HorizontalDirection * _moveSpeed, _rigidbody2D.velocity.y);
+
+            _rigidbody2D.velocity = movement;
+        }
+
+        private void Flip()
+        {
+            if (m_IsFacingRight && m_HorizontalDirection < 0f || !m_IsFacingRight && m_HorizontalDirection > 0f)
+            {
+                m_IsFacingRight = !m_IsFacingRight;
+
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+
+                transform.localScale = localScale;
+            }
+        }
+    }
+}
